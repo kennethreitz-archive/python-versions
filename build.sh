9032ee3c1cfcb030a48b5adb6910ca67b0deb580
Kenneth Reitz <me@kennethreitz.com>
2012-08-05 19:39:42 -0400

    build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Usage: $ build.sh <output-dir
 
 SOURCE_TARBALL='http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2'
-curl $SOURCE_TARBALL | tar jxf
+curl $SOURCE_TARBALL | tar jx
 mv Python-2.7.3 src
 cd src
 
